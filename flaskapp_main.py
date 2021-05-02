016af164000b7549ed58e39f0de6920d06308272
niranjanstudy06 <niranjanstudy06@gmail.com>
2021-05-02 13:08:55 +0530

    update

--- flaskapp_main.py
+++ flaskapp_main.py
@@ -21,11 +21,11 @@ app = Flask(__name__)
 
 #Function to connect
 def connect_mqtt():
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                print("Connected to MQTT Broker!")
-            else:
-                print("Failed to connect, return code %d\n", rc)
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
