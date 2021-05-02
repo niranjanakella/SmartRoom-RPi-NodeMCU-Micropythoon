d6c9739678fc45bdd5ba9c3a4992697a729a947e
niranjanstudy06 <niranjanstudy06@gmail.com>
2021-05-02 13:04:00 +0530

    update

--- flaskapp_main.py
+++ flaskapp_main.py
@@ -5,54 +5,54 @@ from flask import request
 import paho.mqtt.client as mqtt_client
 import time
 
-# broker = '134.209.153.73'
-# port = 1883
-# topic = "test"
-# # generate client ID with pub prefix randomly
-# client_id = 'localhost'
-# username = 'root'
-# password = 'Smart32Room'
+broker = '134.209.153.73'
+port = 1883
+topic = "test"
+
+# generate client ID with pub prefix randomly
+client_id = 'localhost'
+username = 'root'
+password = 'Smart32Room'
 
 SmartRoom_State = 'PowerOFF'
 SmartRoom_json= {'SmartRoom_State':SmartRoom_State}
 app = Flask(__name__)
 
+
+#Function to connect
+def connect_mqtt():
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+#Function to publish message to MQTT Broker
+def publish(client,message):
+        msg_count = 0
+
+        time.sleep(1)
+        result = client.publish(topic, message)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{message}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
 @app.route('/')
 def hello_world():
-    
 
 
-    # def connect_mqtt():
-    #     def on_connect(client, userdata, flags, rc):
-    #         if rc == 0:
-    #             print("Connected to MQTT Broker!")
-    #         else:
-    #             print("Failed to connect, return code %d\n", rc)
-
-    # client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
-    # client.on_connect = on_connect
-    # client.connect(broker, port)
-    # return client
-
-
-    # def publish(client,message):
-    #     msg_count = 0
-
-    #     time.sleep(1)
-    #     result = client.publish(topic, message)
-    #     # result: [0, 1]
-    #     status = result[0]
-    #     if status == 0:
-    #         print(f"Send `{message}` to topic `{topic}`")
-    #     else:
-    #         print(f"Failed to send message to topic {topic}")
-    #     msg_count += 1
-
-
-
-    # mqtt_client = connect_mqtt()
-    # publish(mqtt_client,'Yo Bros This is Awesome')
+    mqtt_client = connect_mqtt()
+    publish(mqtt_client,'Yo Bros This is Awesome')
 
     return 'Hello from SmartRoom!'
 
